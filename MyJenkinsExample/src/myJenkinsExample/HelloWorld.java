63e4c0ea2d0475732abba0f6e0e1c1b864e50e58
Dennis Markmann <dennismarkmann@hotmail.de>
2016-11-22 12:19:03 +0100

    test

--- MyJenkinsExample/src/myJenkinsExample/HelloWorld.java
+++ MyJenkinsExample/src/myJenkinsExample/HelloWorld.java
@@ -3,6 +3,6 @@ package myJenkinsExample;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World2");
+        System.out.println("Hello World");
     }
 }
