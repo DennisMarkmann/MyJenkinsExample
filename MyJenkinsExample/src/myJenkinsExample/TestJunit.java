ebf3a28f78ace4e66e8b2f681882867ab4fb9162
Dennis Markmann <dennismarkmann@hotmail.de>
2016-11-22 21:18:31 +0100

    Fixed Test

--- MyJenkinsExample/src/myJenkinsExample/TestJunit.java
+++ MyJenkinsExample/src/myJenkinsExample/TestJunit.java
@@ -1,13 +1,14 @@
+package myJenkinsExample;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestJunit {
 	
    String message = "Hello World";	
-   MessageUtil messageUtil = new MessageUtil(message);
 
    @Test
    public void testPrintMessage() {
-      assertEquals(message,messageUtil.printMessage());
+      assertEquals("Hello World",message);
    }
 }
